b0202caf1e1633548092f62e272b0f9104a4930c
wassem7 <waseemdarkwa@gmail.com>
2023-11-22 11:54:01 +0000

    Single Respnsibilty

--- SingleResponsibilty.cs
+++ SingleResponsibilty.cs
@@ -1,8 +1,102 @@
 ﻿
 class Logger
 {
-    public void log()
+    public void LogInformation()
     {
-        Console.WriteLine("Info Logged to DB");
+        Console.WriteLine("Attendace Logged to DB");
+    }
+}
+
+class MailSender
+{
+    public void SendAttendanceEmail()
+    {
+        Console.WriteLine("Attendance Email sent");
+    }
+}
+
+class User
+{
+    private string _username;
+
+    public User(string username)
+    {
+        _username = username;
+    }
+
+    public string UserName
+    {
+        get { return this._username; }
+
+        set { this._username = value; }
+
+    }
+
+}
+
+class Validator
+{
+    public void ValidateUser(User user)
+    {
+        Console.WriteLine($"User : {user.UserName} Validated !");
+    }
+}
+
+class ForumRegisteration
+{
+
+    private readonly Validator _validator;
+    private readonly MailSender _mailsender;
+    private readonly Logger _logger;
+
+    public ForumRegisteration()
+    {
+        _validator = new Validator();
+        _mailsender = new MailSender();
+        _logger = new Logger();
+    }
+
+
+
+    public void RegisterUserForForum(User user)
+    {
+        _validator.ValidateUser(user);
+        Console.WriteLine("User Registered !");
+        _logger.LogInformation();
+    }
+
+    static void Main()
+    {
+        ForumRegisteration F1 = new ForumRegisteration();
+        User user = new User("seemzy");
+        F1.RegisterUserForForum(user);
+    }
+}
+
+
+class AttendanceSystem
+{
+    private readonly MailSender _email;
+    private readonly Logger _logger;
+
+    public AttendanceSystem()
+    {
+        _email = new MailSender();
+        _logger = new Logger();
+    }
+
+    public void CreateAttendance()
+    {
+        Console.WriteLine("Attendance Created !");
+        _email.SendAttendanceEmail();
+        _logger.LogInformation();
+
+    }
+
+
+    static void main()
+    {
+        AttendanceSystem a = new AttendanceSystem();
+        a.CreateAttendance();
     }
 }
